f54bbb79f3b08f4ff6c7ea470fa5a52276db3acb
BananaWafflez <benaing12@gmail.com>
2024-06-14 17:08:49 -0400

    Game board now checks for row wins

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const gameBoard = (function (){
     const gameArray = 
     [[" ", " ", " "], 
     [" "," ", " "], 
-    [" "," ", " " ]];
+    [" "," ", " "]];
 
     const setBoard = function(index, letter){
         if(index > 9){
@@ -30,6 +30,47 @@ const gameBoard = (function (){
     console.log("Game created!");
     return {gameArray, setBoard, displayBoard}
 })();
+
+const displayController = (function(){
+    const checkRow = (function(gameArray){
+
+        var count = 0;
+
+        for(let i = 0; i < gameArray.length; i++){
+            //hold the first letter in the row
+            var letterhold = gameArray[i][0];
+            //checks every column in the row
+            for(let j = 0; j < gameArray.length; j++){
+
+                if(gameArray[i][j] == letterhold){
+                    count++;
+                    console.log(count);
+                }
+
+            }
+
+            //if count is 3, we have a match
+            if(count == 3){
+                console.log("Someone Won!");
+                return;
+            }
+
+            count = 0;
+        }
+        
+
+
+
+    })
+
+    return{checkRow};
+
+})();
+
+
+gameBoard.setBoard(1, "X");
+gameBoard.setBoard(2, "X");
 gameBoard.setBoard(3, "X");
 gameBoard.displayBoard();
+displayController.checkRow(gameBoard.gameArray);
 
